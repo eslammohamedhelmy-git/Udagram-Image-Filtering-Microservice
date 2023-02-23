2e5757278d056371dbb991e6725b8effbbbed2ea
eslammohamedhelmy-git <70444790+eslammohamedhelmy-git@users.noreply.github.com>
2023-02-23 14:09:43 +0000

    Bug Fix
    I changed the data tybe of image_url

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ import { url } from 'inspector';
 
   //! END @TODO1
   app.get( "/filteredimage", async(req:express.Request, res:express.Response) => {
-    let {image_url} = req.query;
+    const {image_url}: {image_url:string} = req.query;
     if (!image_url){
       res.status(400).send('Error: The submitted url is empty');
     } else {
